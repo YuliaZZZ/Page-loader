ba991de0967fcedcbcd79b844ac8f474ef92ad00
YuliaZZZ <zinowiewa@yandex.ru>
2020-03-27 00:43:57 +0700

    delete lint error in loader

--- loader/start_loader.py
+++ loader/start_loader.py
@@ -4,6 +4,7 @@ from loader import engine
 
 parser = argparse.ArgumentParser(description='Page loader')
 parser.add_argument('site', type=str)
-parser.add_argument('-o', '--output', type=str, default='.', help='folder to save page')
+parser.add_argument('-o', '--output', type=str, default='.',
+                    help='folder to save page')
 args = parser.parse_args()
 file_new = engine.page_load(args.site, args.output)
