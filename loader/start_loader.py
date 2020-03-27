cdbe6944e281120033a9661a5e6139e539759320
YuliaZZZ <zinowiewa@yandex.ru>
2020-03-27 22:59:59 +0700

    try tests

--- loader/start_loader.py
+++ loader/start_loader.py
@@ -2,9 +2,9 @@ import argparse
 from loader import engine
 
 
-parser = argparse.ArgumentParser(description='Page loader')
-parser.add_argument('site', type=str)
+parser = argparse.ArgumentParser(description='Page loader')     # pragma: no cover
+parser.add_argument('site', type=str)      # pragma: no cover
 parser.add_argument('-o', '--output', type=str, default='.',
-                    help='folder to save page')
-args = parser.parse_args()
-file_new = engine.page_load(args.site, args.output)
+                    help='folder to save page')            # pragma: no cover
+args = parser.parse_args()     # pragma: no cover
+file_new = engine.page_load(args.site, args.output)      # pragma: no cover
