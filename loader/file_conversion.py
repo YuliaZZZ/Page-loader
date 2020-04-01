40b6ea10fa3e3d5cb9c3afe71094b9c6f0335891
YuliaZZZ <zinowiewa@yandex.ru>
2020-04-02 02:44:00 +0700

    fix step2

--- loader/file_conversion.py
+++ loader/file_conversion.py
@@ -7,13 +7,15 @@ def change_html(html_file, catalog):
     items_src = []
     with open(html_file) as fp:
         soup = BeautifulSoup(fp, 'xml')
-        for i in soup.find_all(['script', 'img'], src=re.compile("^[/|//]")):
-            items_src.append(i['src'])
-            i['src'] = create_name_file(items_src[-1], catalog, head=1)
-        for j in soup.find_all('link', href=re.compile("^/")):
-            items_src.append(j['href'])
-            j['href'] = create_name_file(items_src[-1], catalog, head=1)
-        text = str(soup)
+        for i in (soup.find_all(['script', 'img'], src=re.compile("^[/|//]")) +
+                  soup.find_all('link', href=re.compile("^/"))):
+            if i.name == 'src':
+                items_src.append(i['src'])
+                i['src'] = create_name_file(items_src[-1], catalog, head=1)
+            elif i.name == 'link':
+                items_src.append(i['href'])
+                i['href'] = create_name_file(items_src[-1], catalog, head=1)
+    text = str(soup)
     with open(html_file, 'w') as fp:
         fp.write(text)
     return items_src
